68f16be40f43a08b98e6b8528a7f28942e4fb13a
8Balu8 <barzagliluca@gmail.com>
2022-01-10 19:02:53 +0100

    add main with disc-card

--- src/components/DiscCard.vue
+++ src/components/DiscCard.vue
@@ -1,21 +1,24 @@
 <template>
     <div class="card">
-        <img src="https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg" alt="">
+        <img :src="contentOfCard.poster" :alt="contentOfCard.author">
         <div class="title">
-            Sono il titolo
+            {{contentOfCard.title}}
         </div>
         <div class="disc-info">
-            Author
+            {{contentOfCard.author}}
         </div>
         <div class="disc-info">
-            Year
+            {{contentOfCard.year}}
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "DiscCard"
+    name: "DiscCard",
+    props: {
+        contentOfCard: Object
+    }
 }
 </script>
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,13 +2,7 @@
     <main>
         <div class="container">
             <div class="card-contain">
-                <DiscCard/>
-                <DiscCard/>
-                <DiscCard/>
-                <DiscCard/>
-                <DiscCard/>
-                <DiscCard/>
-                <DiscCard/>
+                <DiscCard v-for="element, index in discListArray" :key="index" :contentOfCard="element"/>
             </div>
         </div>
     </main>
@@ -16,11 +10,27 @@
 
 <script>
 import DiscCard from "./DiscCard.vue";
+import axios from "axios";
 
 export default {
     name:"Main",
     components: {
         DiscCard
+    },
+    data: function() {
+        return {
+            discListArray: []
+        };
+    },
+    methods: {
+        getDiscs: function () {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
+                this.discListArray = response.data.response
+            })
+        }
+    },
+    created: function() {
+        this.getDiscs()
     }
 }
 </script>
