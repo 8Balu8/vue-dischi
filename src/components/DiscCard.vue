18bdae40371ca081ca0bf306d0280acfc6ee5640
8Balu8 <barzagliluca@gmail.com>
2022-01-10 17:05:19 +0100

    end layout

---
+++ src/components/DiscCard.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="card">
+        <img src="https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg" alt="">
+        <div class="title">
+            Sono il titolo
+        </div>
+        <div class="disc-info">
+            Author
+        </div>
+        <div class="disc-info">
+            Year
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DiscCard"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+
+.card {
+    background-color: $card-color;
+    width: calc((100% / 5) - 40px);
+    margin: 10px 20px;
+    padding: 20px;
+    text-align: center;
+
+    img {
+	    width: 100%;
+    }
+
+    .title {
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 20px;
+        margin: 10px 0;
+    }
+
+    .disc-info {
+        color: gray;
+    }
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,41 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="card-contain">
+                <DiscCard/>
+                <DiscCard/>
+                <DiscCard/>
+                <DiscCard/>
+                <DiscCard/>
+                <DiscCard/>
+                <DiscCard/>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+import DiscCard from "./DiscCard.vue";
+
+export default {
+    name:"Main",
+    components: {
+        DiscCard
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables";
+main {
+    padding-top: 30px;
+    height: calc(100vh - 50px);
+    background-color: $main-color;
+    color: white;
+
+    .card-contain {
+        display: flex;
+        flex-wrap: wrap;
+    }
+}
+</style>
