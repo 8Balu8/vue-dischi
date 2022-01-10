f933bfe7232923b761d7d117ac5a364b9bc9af0c
8Balu8 <barzagliluca@gmail.com>
2022-01-10 15:44:25 +0100

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <img src="https://thumbs.dreamstime.com/b/modalit%C3%A0-scura-logo-spotify-vinnytsia-ucraina-maggio-un-servizio-di-musica-cool-che-offre-flusso-streaming-legale-218033923.jpg" alt="Spotify Logo">
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+    height: 50px;
+    padding: 20px;
+    background-color: black;
+    display: flex;
+    align-items: center;
+
+    img {
+        height: 30px;
+    }
+}
+</style>
